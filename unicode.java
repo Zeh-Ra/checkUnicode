adcc7f9830f633a645d9bb98133df78f7c49045d
Zehra Celik <114779939+Zeh-Ra@users.noreply.github.com>
2024-12-07 11:03:49 +0100

    Add files via upload

---
+++ unicode.java
@@ -0,0 +1,12 @@
+package unicode;
+
+public class unicode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("uafuÃ¤Ã¶Ã¼");
+		System.out.println("äöü");
+		System.out.println("안");
+		
+	}
+}
